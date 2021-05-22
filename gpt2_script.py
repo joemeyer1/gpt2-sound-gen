7b54edfd02a0c46f532e6c4ff873f1ca4fc9d9a1
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2021-05-22 00:26:06 -0700

    bug fix - consistent vocab size

--- gpt2_script.py
+++ gpt2_script.py
@@ -6,11 +6,14 @@ from aitextgen.utils import build_gpt2_config
 
 from make_wav_str_file import convert_wav_to_text_file
 
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-def run_gpt2_script(
-    steps: int = 100,
-    wav_str_filename: str = "sound.txt"
-) -> None:
+
+def run_gpt2_script(**kwargs) -> None:
+
+    steps: int = kwargs.get("steps", 100)
+    wav_str_filename: str = kwargs.get("wav_str_filename", "sound.txt")
 
     convert_wav_to_text_file(
         in_wav_dir_name="sound_files",
@@ -19,10 +22,11 @@ def run_gpt2_script(
     )
 
     print(f"Tokenizing file name {wav_str_filename}")
-    train_tokenizer(wav_str_filename)
+    n_tokens = len(set(TokenDataset(wav_str_filename, block_size=32).tokens))  # can also be arbitrary int, e.g. 1000
+    train_tokenizer(wav_str_filename, vocab_size=n_tokens)
 
     config = build_gpt2_config(
-        vocab_size=len(set(TokenDataset(wav_str_filename, block_size=32).tokens)),
+        vocab_size=n_tokens,
         max_length=32,
         dropout=0.0,
         n_embd=256,
@@ -31,7 +35,7 @@ def run_gpt2_script(
     )
     ai = aitextgen(tokenizer_file="aitextgen.tokenizer.json", config=config)
 
-    print(f"Training ({steps} epochs}")
+    print(f"Training ({steps} epochs")
     ai.train(wav_str_filename, batch_size=16, num_steps=steps)
     ai.generate(n=100, prompt="")
 
