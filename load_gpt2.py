0ca6f1a5cebcd7452d23a862b6720b0932179c30
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2021-05-22 06:47:44 -0700

    fix typo

--- load_gpt2.py
+++ load_gpt2.py
@@ -1,8 +1,8 @@
 
 from aitextgen import aitextgen
 
-def load_gpt2(model_folderr="trained_model", tokenizer_file="aitextgen.tokenizer.json", prompt=""):
-    ai = aitextgen(model_folder=model_folderr, tokenizer_file=tokenizer_file,)
+def load_gpt2(model_folder="trained_model", tokenizer_file="aitextgen.tokenizer.json", prompt=""):
+    ai = aitextgen(model_folder=model_folder, tokenizer_file=tokenizer_file,)
     ai.generate(n=1, batch_size=100, max_length=1000, prompt=prompt, return_as_list=False)
 
 if __name__ == "__main__":
