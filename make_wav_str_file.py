bc02f3f5540b24df7901603af9aec73439eace06
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2021-05-21 21:12:02 -0700

    style improvements

--- make_wav_str_file.py
+++ make_wav_str_file.py
@@ -9,9 +9,9 @@
 from model_files.data_fetcher import get_training_data
 from vec_to_wav import int_to_hex
 
-def write_file_from_wav_str(
-        read_wav_from_dir: str = "sound_files",
-        write_wav_str_to_file: str = "sound.txt",
+def convert_wav_to_text_file(
+        in_wav_dir_name: str = "sound_files",
+        out_text_filename: str = "sound.txt",
         n_max_files: int = 1,
 ) -> None:
 
@@ -24,7 +24,7 @@ def write_file_from_wav_str(
         return tokens_list
 
     # vec_int_to_hex = np.vectorize(lambda i: int_to_hex(int_to_convert=i, bytes=4))
-    ints_data = get_training_data(read_wav_from_dir=read_wav_from_dir, n_max_files=n_max_files)
+    ints_data = get_training_data(read_wav_from_dir=in_wav_dir_name, n_max_files=n_max_files)
     # tokens = tokens.flatten()
     # # bucketize tokens to avoid having like tens of thousands
     # mass, bins = np.histogram(a=tokens, bins=16)
@@ -36,5 +36,5 @@ def write_file_from_wav_str(
             new_tokens += hex_to_tokens(int_to_hex(int_to_convert=token, bytes=4)) + ['-']
         new_tokens.append('<END>')
     tokens_str = "".join(map(str, new_tokens))
-    with open(write_wav_str_to_file, 'w') as f:
+    with open(out_text_filename, 'w') as f:
         f.write(tokens_str)
