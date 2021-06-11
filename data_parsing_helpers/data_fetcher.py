563b45e4ccce77fcbf74b93fe699f19d85e3c1b8
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2021-06-11 00:16:04 -0700

    make it work w real data

--- data_parsing_helpers/data_fetcher.py
+++ data_parsing_helpers/data_fetcher.py
@@ -22,12 +22,14 @@ def get_training_data(read_wav_from_dir: str, n_max_files: int = 0) -> np.array:
     for filename in filenames[-n_max_files:]:
         read_wav_from_path = f"{read_wav_from_dir}/{filename}"
         data_channels, _ = extract_data(read_wav_from_filename=read_wav_from_path)
-        flat_data_channels = np.array(list(data_channels.values())).flatten()
+        # flat_data_channels = np.array(list(data_channels.values())).flatten()
+        flat_data_channels = data_channels[0]
         vectors_list.append(flat_data_channels)
     biggest_vector_len = max([len(vec) for vec in vectors_list])
     for i in range(len(vectors_list)):
         padding = [0] * (biggest_vector_len - len(vectors_list[i]))
-        vectors_list[i] = np.concatenate((vectors_list[i], padding))
+        if padding:
+            vectors_list[i] = np.concatenate((vectors_list[i], padding))
     vectors = np.stack(vectors_list)
     return vectors
 
@@ -36,4 +38,4 @@ def get_training_data(read_wav_from_dir: str, n_max_files: int = 0) -> np.array:
 
 if __name__ == "__main__":
     # main("test.wav")
-    get_training_data("sound_files")
+    get_training_data("../sound_data", n_max_files=2)
--- train_lstm.py
+++ train_lstm.py
@@ -1,25 +1,40 @@
 
 import fire
+import numpy as np
 import torch
 from lstm_with_head import LSTMWithHead as LSTMWithHead
+from data_parsing_helpers.data_fetcher import get_training_data
 
-def train_lstm():
+def train_lstm(
+    epochs: int = 100,
+    n_max_files: int = 1,
+    in_wav_dir_name: str = "sound_data",
+    wav_str_filename: str = "sound_short.txt",
+    use_previous_training_data: bool = True,
+    learning_rate=1000,
+    load_model_from_chkpt=None,
+    save_model_every_n_epochs=1000,
+    overwrite_previous_model=False,
+):
 
     # get data
     input_size = 1
     output_size = 1
     hidden_size = 20
     num_layers = 2
-    batch_size = 1
-    seq_length = 5
-    features = torch.randn(seq_length, batch_size, input_size)
-    labels = torch.cat((features[1:], torch.tensor([[[5]]])), 0)
+    # batch_size = 1
+    # seq_length = 5
+    # features = torch.randn(seq_length, batch_size, input_size)
+    # labels = torch.cat((features[1:], torch.tensor([[[5]]])), 0)
+    data = torch.tensor(np.expand_dims(get_training_data(read_wav_from_dir="sound_data", n_max_files=5), -1).astype(np.float32))
+    # data = torch.tensor([[[1], [2], [3], [4]]]).float()
+    features = data[:, :-1]
+    labels = data[:, 1:]
+    batch_size, seq_length, input_size = features.shape
 
     # get net
     net = LSTMWithHead(input_size=input_size, output_size=output_size, hidden_size=hidden_size, num_layers=num_layers, )
-    epochs = 1000
-    lr = .01
-    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
     loss_fn = torch.nn.MSELoss()
 
     for i in range(epochs):
