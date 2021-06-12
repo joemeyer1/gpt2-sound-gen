63a1b2be07e94bd836503b0169682d2e1c658218
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2021-06-12 16:36:43 -0700

    save net, make cleaner

--- train_lstm.py
+++ train_lstm.py
@@ -11,11 +11,11 @@ from generate_gpt2_text import write_wav
 def train_lstm(
     epochs: int = 10,
     n_max_files: int = 1,
-    in_wav_dir_name: str = "sound_data",
+    in_wav_dir_name: str = "sound_data_percussion",
     wav_str_filename: str = "sound_short.txt",
     use_previous_training_data: bool = True,
     learning_rate=.01,
-    output_wav_len=100000,
+    output_wav_len=100,
     load_model_from_chkpt=None,
     save_model_every_n_epochs=1000,
     overwrite_previous_model=False,
@@ -24,13 +24,13 @@ def train_lstm(
     # get data
     input_size = 1
     output_size = 1
-    hidden_size = 20
-    num_layers = 2
+    hidden_size = 64
+    num_layers = 4
     # batch_size = 1
     # seq_length = 5
     # features = torch.randn(seq_length, batch_size, input_size)
     # labels = torch.cat((features[1:], torch.tensor([[[5]]])), 0)
-    data = torch.tensor(np.expand_dims(get_training_data(read_wav_from_dir="sound_data", n_max_files=5), -1).astype(np.float32))
+    data = torch.tensor(np.expand_dims(get_training_data(read_wav_from_dir=in_wav_dir_name, n_max_files=n_max_files), -1).astype(np.float32))
     # data = torch.tensor([[[1], [2], [3], [4]]]).float()
 
     # normalize data
@@ -59,21 +59,22 @@ def train_lstm(
     except Exception as e:
         print(e)
 
-    prompt = torch.tensor([[[0]]], dtype=torch.float)
+    generated_wav_body = torch.tensor([[[0]]], dtype=torch.float)
     hncn = None
     for i in range(output_wav_len - 1):
-        y_pred, hncn = net(prompt, hncn)
-        prompt = torch.cat((prompt, y_pred[-1:]), axis=0)
-    print(prompt)
-    prompt = (prompt * torch.mean(data_std)) + torch.mean(data_avg)
-    print(prompt)
+        next_input = generated_wav_body[-1:]
+        y_pred, hncn = net(next_input, hncn)
+        generated_wav_body = torch.cat((generated_wav_body, y_pred[-1:]), axis=0)
+    print(generated_wav_body)
+    generated_wav_body = (generated_wav_body * torch.mean(data_std)) + torch.mean(data_avg)
+    print(generated_wav_body)
 
     def int_to_hex_str(int_to_convert, n_bits):
         ret = hex(int_to_convert)[2:]
         ret = '0' * (n_bits - len(ret)) + ret
         return ret
 
-    hex_prompt = [int_to_hex_str(int_to_convert=floor(i), n_bits=8) for i in prompt.flatten()]
+    hex_prompt = [int_to_hex_str(int_to_convert=floor(i), n_bits=8) for i in generated_wav_body.flatten()]
     wav_body = ""
     for h in hex_prompt:
         wav_body += h
@@ -81,6 +82,7 @@ def train_lstm(
     write_wav_to_filename = "trash.wav"
     print(f"writing wav file '{write_wav_to_filename}'")
     write_wav(wav_txt=wav_body, write_wav_to_filename=write_wav_to_filename)
+    torch.save(net, 'lstm.pt')
 
 
 if __name__ == "__main__":
