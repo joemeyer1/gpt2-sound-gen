1a9bc716ae32b31176fa82a929eeb60c4c4f6863
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2023-07-29 06:34:25 -0700

    Update quick_train.py

--- quick_train.py
+++ quick_train.py
@@ -6,16 +6,16 @@ from train_model import train_model
 
 def quick_train():
     train_model(
-        steps=100,
+        steps=1000,
         n_max_files=10000,
         in_wav_dir_name="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_percussion",
         wav_str_filename="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_output_percussion/generated_percussion_all.txt",
         output_dir="/Users/joemeyer/Documents/gpt2-sound-gen/trained_percussion_model",
-        tokenizer_name='percussion_tokenizer',
-        use_previous_training_data=False,
+        tokenizer_name='percussion_tokenizer0',
+        use_previous_training_data=True,
         learning_rate=1e-3,
-        load_model_from_chkpt=None,
-        save_model_every_n_epochs=10,
+        load_model_from_chkpt="trained_percussion_model2",
+        save_model_every_n_epochs=50,
         overwrite_previous_model=False,
     )
 
