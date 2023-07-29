4c74b8a57ef39a6d41d7246730e62311a3e2976b
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2023-07-29 04:23:29 -0700

    train/gen params

--- quick_generate.py
+++ quick_generate.py
@@ -6,12 +6,12 @@ from generate_gpt2_text import generate_wav
 
 def test_generate_gpt2():
     generate_wav(
-        model_folder="trained_model2",
-        tokenizer_file="aitextgen000.tokenizer.json",
+        model_folder="trained_percussion_model0",
+        tokenizer_file="percussion_tokenizer0.tokenizer.json",
         prompt="",
-        min_audio_length=100,
+        min_audio_length=1000,
         window_length=32,
-        write_wav_to_filename="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_output_toy/test_generate.wav",
+        write_wav_to_filename="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_output_percussion/generated_percussion.wav",
         overwrite_previous_model_data=False,
         num_channels=1,
         sample_rate=48000,
--- quick_train.py
+++ quick_train.py
@@ -7,14 +7,14 @@ from train_gpt2 import train_gpt2 as train
 def quick_train():
     train(
         steps=100,
-        n_max_files=1,
+        n_max_files=10000,
         in_wav_dir_name="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_percussion",
         wav_str_filename="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_output_percussion/generated_percussion.txt",
         output_dir="/Users/joemeyer/Documents/gpt2-sound-gen/trained_percussion_model",
         tokenizer_name='/Users/joemeyer/Documents/gpt2-sound-gen/percussion_tokenizer',
         use_previous_training_data=False,
         learning_rate=1e-3,
-        load_model_from_chkpt=None,
+        load_model_from_chkpt="/Users/joemeyer/Documents/gpt2-sound-gen/trained_percussion_model",
         save_model_every_n_epochs=10,
         overwrite_previous_model=False,
     )
