107cfe8cd4bf69c5b3dd8707230993ac9809e65a
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2023-07-29 04:31:59 -0700

    Update quick_train.py
    save tokenizer in model folder

--- quick_train.py
+++ quick_train.py
@@ -1,20 +1,20 @@
 #!usr/bin/env python3
 # Copyright (c) Joseph Meyer. All rights reserved.
 
-from train_gpt2 import train_gpt2 as train
+from train_gpt2 import train_gpt2
 
 
 def quick_train():
-    train(
+    train_gpt2(
         steps=100,
         n_max_files=10000,
         in_wav_dir_name="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_percussion",
         wav_str_filename="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_output_percussion/generated_percussion.txt",
         output_dir="/Users/joemeyer/Documents/gpt2-sound-gen/trained_percussion_model",
-        tokenizer_name='/Users/joemeyer/Documents/gpt2-sound-gen/percussion_tokenizer',
-        use_previous_training_data=False,
+        tokenizer_name='/Users/joemeyer/Documents/gpt2-sound-gen/trained_percussion_model/percussion_tokenizer',
+        use_previous_training_data=True,
         learning_rate=1e-3,
-        load_model_from_chkpt="/Users/joemeyer/Documents/gpt2-sound-gen/trained_percussion_model",
+        load_model_from_chkpt=None,
         save_model_every_n_epochs=10,
         overwrite_previous_model=False,
     )
