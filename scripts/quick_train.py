b04a55f370f02fb7915d8c22231a54d98447bc88
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2023-08-07 15:52:12 -0700

    scripts folder

--- scripts/quick_train.py
+++ scripts/quick_train.py
@@ -11,7 +11,7 @@ def quick_train():
         in_wav_dir_name="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_strings",
         formatted_training_data_filename="/Users/joemeyer/Documents/gpt2-sound-gen/formatted_training_data_strings/generated_strings_short_model3.txt",
         output_dir="/Users/joemeyer/Documents/gpt2-sound-gen/trained_strings_model",
-        tokenizer_name='strings_tokenizer_short0',
+        tokenizer_name='/Users/joemeyer/Documents/gpt2-sound-gen/strings_tokenizer_short0',
         use_previous_training_data=False,
         learning_rate=1e-5,
         load_model_from_chkpt="trained_strings_model2",
