80cd3833d3b681cd4cd283e33021e86299d4f2ac
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2023-08-07 15:14:00 -0700

    use base_model_dir
    if there's a '0' on the end, it will add another '0' to make unique instead of incrementing by 1. You're not supposed to pass the numeric suffix (under current design)

--- cycle_train_gen.py
+++ cycle_train_gen.py
@@ -7,27 +7,29 @@ from train_gpt2 import train_gpt2, ModelData
 
 def test_cycle_train_and_generate(n_cycles: int = 4):
 
+    base_model_dir = "/Users/joemeyer/Documents/gpt2-sound-gen/trained_strings_model_cycle"
     model_data = ModelData(
         tokenizer_filename='strings_tokenizer_cycle',
-        model_dir="/Users/joemeyer/Documents/gpt2-sound-gen/trained_strings_model_cycle",
+        model_dir=base_model_dir,
     )
     load_model_from_chkpt = None
     use_previous_training_data = False
 
     for i in range(n_cycles):
         print('\n\nTRAINING::\n')
-        steps: int = 10 if i != 0 else 0
+        steps: int = 2 if i != 0 else 0
+        print(f"model_data.model_dir: {model_data.model_dir}\n")
         model_data: ModelData = train_gpt2(
             steps=steps,
-            n_max_files=10,
+            n_max_files=2,
             in_wav_dir_name="/Users/joemeyer/Documents/gpt2-sound-gen/sound_data_strings",
             formatted_training_data_filename="/Users/joemeyer/Documents/gpt2-sound-gen/formatted_training_data_strings/generated_strings_short_cycle_model.txt",
-            output_dir=model_data.model_dir,
+            output_dir=base_model_dir,
             tokenizer_name=model_data.tokenizer_filename,
             use_previous_training_data=use_previous_training_data,
             learning_rate=1e-3,
             load_model_from_chkpt=load_model_from_chkpt,
-            save_model_every_n_epochs=10,
+            save_model_every_n_epochs=2,
             overwrite_previous_model=False,
             block_size=1024,
         )
